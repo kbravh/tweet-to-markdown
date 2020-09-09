a3483a75730c965f4f14d07ea3759d8738beeb5d
Karey Higuera <karey.higuera@gmail.com>
2020-09-09 10:39:53 -0500

    move get tweet to util

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const { default: Axios } = require("axios")
 const commandLineArgs = require(`command-line-args`)
 const commandLineUsage = require(`command-line-usage`)
 const clipboard = require(`clipboardy`)
@@ -84,38 +83,6 @@ if (!bearer) {
 
 // extract tweet ID from URL ID
 let id = util.getTweetID(options)
-
-// fetch tweet from Twitter API
-/**
- * Fetches a tweet object from the Twitter v2 API
- * @param {string} id - The ID of the tweet to fetch from the API
- */
-const getTweet = async id => {
-  let twitterUrl = new URL(`https://api.twitter.com/2/tweets/${id}`)
-  let params = new URLSearchParams({
-    "expansions": "author_id,attachments.poll_ids,attachments.media_keys",
-    "user.fields": "name,username,profile_image_url",
-    "tweet.fields": "attachments,public_metrics,entities",
-    "media.fields": "url",
-    "poll.fields": "options"
-  })
-
-  return await Axios({
-    method: `GET`,
-    url: `${twitterUrl.href}?${params.toString()}`,
-    headers: { 'Authorization': `Bearer ${bearer}` }
-  })
-    .then(response => response.data)
-    .catch(error => {
-      if (error.response) {
-        panic(chalk.red(error.response.statusText))
-      } else if (error.request) {
-        panic(chalk.red(`There seems to be a connection issue.`))
-      } else {
-        panic(chalk.red(`An error occurred.`))
-      }
-    })
-}
 
 /**
  * 
@@ -305,7 +272,7 @@ const copyToClipboard = async markdown => {
 }
 
 const main = async () => {
-  let tweet = await getTweet(id)
+  let tweet = await util.getTweet(id, bearer)
   let markdown = buildMarkdown(tweet)
   if(options.clipboard){
     copyToClipboard(markdown)
--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+const { default: Axios } = require("axios")
+
 /**
  * Parses out the tweet ID from the URL or ID that the user provided
  * @param {options} options - The parsed command line arguments
@@ -14,6 +16,39 @@ const getTweetID = ({src}) => {
   return id
 }
 
+// fetch tweet from Twitter API
+/**
+ * Fetches a tweet object from the Twitter v2 API
+ * @param {string} id - The ID of the tweet to fetch from the API
+ */
+const getTweet = async (id, bearer) => {
+  let twitterUrl = new URL(`https://api.twitter.com/2/tweets/${id}`)
+  let params = new URLSearchParams({
+    "expansions": "author_id,attachments.poll_ids,attachments.media_keys",
+    "user.fields": "name,username,profile_image_url",
+    "tweet.fields": "attachments,public_metrics,entities",
+    "media.fields": "url",
+    "poll.fields": "options"
+  })
+
+  return await Axios({
+    method: `GET`,
+    url: `${twitterUrl.href}?${params.toString()}`,
+    headers: { 'Authorization': `Bearer ${bearer}` }
+  })
+    .then(response => response.data)
+    .catch(error => {
+      if (error.response) {
+        panic(chalk.red(error.response.statusText))
+      } else if (error.request) {
+        panic(chalk.red(`There seems to be a connection issue.`))
+      } else {
+        panic(chalk.red(`An error occurred.`))
+      }
+    })
+}
+
 module.exports = {
-  getTweetID
+  getTweetID,
+  getTweet
 }
